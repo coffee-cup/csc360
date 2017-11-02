954a214c1df974c4d077d425afd18a5d71715de1
Jake Runzer <jakerunzer@gmail.com>
2017-11-02 02:10:08 -0700

    customers flow

--- a2/submission/main.c
+++ a2/submission/main.c
@@ -10,6 +10,7 @@
 #define NUM_QUEUES 4
 
 CustomerQueue *queues[NUM_QUEUES];
+int total_customers_remaining = 0;
 
 // Mutexes and Condition variables
 pthread_mutex_t queue_lock;
@@ -58,7 +59,7 @@ void get_longest_queue(CustomerQueue **queue, int *index, int *length) {
   *length = longest_length;
 }
 
-void *enqueue_with_customer(Customer *customer) {
+void enqueue_customer(Customer *customer) {
   int index;
   int length;
   CustomerQueue *queue;
@@ -67,7 +68,9 @@ void *enqueue_with_customer(Customer *customer) {
 
   get_shortest_queue(&queue, &index, &length);
   enqueue(queue, customer);
-  print_queue_lengths();
+  pthread_cond_signal(&queue_cond);
+
+  // print_queue_lengths();
 
   printf("A customer enters a queue: the queue ID %1d, and length of the queue "
          "%1d. \n",
@@ -76,24 +79,57 @@ void *enqueue_with_customer(Customer *customer) {
   pthread_mutex_unlock(&queue_lock);
 }
 
+void process_customer(Customer *customer, int clerk_id) {
+  printf("A clerk starts serving a customer: start time , the customer ID %2d, "
+         "the clerk ID %1d. \n",
+         customer->id, clerk_id);
+
+  usleep(customer->service_time * 100000);
+
+  printf("A clerk finishes serving a customer: end time, the customer ID %2d, "
+         "the clerk ID %1d. \n",
+         customer->id, clerk_id);
+}
+
 void *customer_thread(void *customer_pointer) {
   Customer *customer = (Customer *)customer_pointer;
-  printf("Customer thread %d\n", customer->id);
+  // printf("Customer thread %d\n", customer->id);
 
   usleep(customer->arrival_time * 100000);
 
   printf("A customer arrives: customer ID %2d. \n", customer->id);
-  enqueue_with_customer(customer);
+  enqueue_customer(customer);
 
   return NULL;
 }
 
-void *clerk_thread() {
-  printf("I am clerk thread\n");
+void *clerk_thread(void *clerk_id_pointer) {
+  int clerk_id = *((int *)clerk_id_pointer);
+  // printf("I am clerk thread %d\n", clerk_id);
 
-  // while (true) {
-  //   pthread_mutex_lock(&queue_lock);
-  // }
+  while (TRUE) {
+    Customer *customer;
+
+    int index;
+    int length;
+    CustomerQueue *queue;
+
+    pthread_mutex_lock(&queue_lock);
+
+    get_longest_queue(&queue, &index, &length);
+    customer = dequeue(queue);
+
+    if (customer == NULL) {
+      printf("Clerk %d waiting\n", clerk_id);
+      pthread_cond_wait(&queue_cond, &queue_lock);
+      customer = dequeue(queue);
+    }
+
+    // print_queue_lengths();
+    pthread_mutex_unlock(&queue_lock);
+
+    process_customer(customer, clerk_id);
+  }
 
   return NULL;
 }
@@ -147,6 +183,8 @@ int main(int argc, char *argv[]) {
   }
   printf("\n");
 
+  total_customers_remaining = num_customers;
+
   // Init pthread
   if (pthread_mutex_init(&queue_lock, NULL) != 0) {
     printf("\n mutex init failed\n");
@@ -161,7 +199,11 @@ int main(int argc, char *argv[]) {
   // Create clerk threds
   pthread_t clerks_threads[NUM_CLERKS];
   for (i = 0; i < NUM_CLERKS; i += 1) {
-    if (pthread_create(&clerks_threads[i], NULL, clerk_thread, NULL)) {
+    int *clerk_id =
+        (int *)malloc(sizeof(int)); // Force compiler to create new variable
+    *clerk_id = i;
+    printf("Creating clerk %d - %p\n", *clerk_id, clerk_id);
+    if (pthread_create(&clerks_threads[i], NULL, clerk_thread, clerk_id)) {
       fprintf(stderr, "Error creating clerk thread %d\n", i);
       return 2;
     }
